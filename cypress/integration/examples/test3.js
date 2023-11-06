c28da85fb9c13014c1728042123c1db1a09a289b
Bramdont García <bramdont@gmail.com>
2023-11-06 16:59:06 -0400

    handling visibility and radio button

--- cypress/integration/examples/test3.js
+++ cypress/integration/examples/test3.js
@@ -1,4 +1,5 @@
 /// <reference types="Cypress" />
+
 describe('Exercise using checkbox, dropdown list, etc', () => {
     it('Using checkbox', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice')
@@ -12,7 +13,7 @@ describe('Exercise using checkbox, dropdown list, etc', () => {
         cy.get("input[type='checkbox']").check(['option2','option3'])
     });
 
-    it('static dropdown list', () => {
+    it('Static dropdown list', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice')
         //Using the method .select() to choose an option from the dropdown list, and verifying the selected option
         cy.get('#dropdown-class-example').select('option2').should('have.value', 'option2')
@@ -23,14 +24,33 @@ describe('Exercise using checkbox, dropdown list, etc', () => {
 
         //Typing to bring options to the dropdown list
         cy.get('input#autocomplete').type('dom')
-
+        //adding a wait method to wait until options are displayed
+        cy.wait(1000)
         //Iterating all values to look for the one we want to choose
         cy.get('.ui-menu-item div').each(($el,index,$list) =>{
             if($el.text()==='Dominican Republic'){
                 cy.wrap($el).click();
             }
         })
-        //Assertion to validate the selected value is Domincan Republick
+        //Assertion to validate the selected value is Domincan Republic
         cy.get('input#autocomplete').should('have.value', 'Dominican Republic')
+    });
+
+    it('Handlying visibility', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice')
+
+        cy.get('#hide-textbox').click()
+        //Assertion to validate if the textbox is not visible after hiding it
+        cy.get('#displayed-text').should('not.be.visible')
+
+        cy.get('#show-textbox').click()
+        //Assertion to validate if the textbox is visible after showing it
+        cy.get('#displayed-text').should('be.visible')
+    });
+
+    it('Selecting radio button', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice')
+
+        cy.get('input[value="radio1"]').check().should('be.checked')
     });
 });
